8c9282fcca3c4f7b9152fc90b99a8dad2379f50a
Daniel Molloy <dmolloy@myonlysolution.com>
2023-05-07 11:59:33 -0400

    added multiple recipient feature

--- Escrow.sol
+++ Escrow.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: LicenseRef-Proprietary
 // see LICENSE in the source repository
-//working version
+// working version
 
 pragma solidity ^0.8.0;
 
@@ -12,7 +12,7 @@ contract Escrow is IEscrow {
     // EscrowInfo struct contains all the details of a specific escrow instance.
     struct EscrowInfo {
         address payable sender;
-        address payable recipient;
+        address payable[] recipients;
         uint256 amount;
         uint256 negotiated_amount;
         bool isFulfilled;
@@ -22,24 +22,38 @@ contract Escrow is IEscrow {
     mapping(uint256 => EscrowInfo) public escrows;
     uint256 public nextEscrowId;
 
-    // Creates a new escrow with the provided recipient and amount, and stores it in the mapping.
-    function createEscrow(address _recipient, uint256 _amount) public payable returns (uint256) {
+    // Creates a new escrow with the provided recipients, amount, and KPIs, and stores it in the mapping.
+    function createEscrow(address[] memory _recipients, uint256 _amount) public payable returns (uint256) {
         require(msg.value == _amount, "Amount sent does not match the specified amount.");
+        require(_recipients.length > 0, "At least one recipient is required.");
 
+        // Create a new EscrowInfo struct and store it in the mapping
         EscrowInfo memory newEscrow = EscrowInfo({
             sender: payable(msg.sender),
-            recipient: payable(_recipient),
+            recipients: _toPayableArray(_recipients),
             amount: _amount,
             negotiated_amount: 0,
             isFulfilled: false
         });
 
         escrows[nextEscrowId] = newEscrow;
-        emit EscrowCreated(nextEscrowId, msg.sender, _recipient, _amount);
+        emit EscrowCreated(nextEscrowId, msg.sender, _amount);
+        for (uint i = 0; i < _recipients.length; i++) {
+            emit RecipientAdded(nextEscrowId, _recipients[i]);
+        }
 
         nextEscrowId++;
         return nextEscrowId - 1;
-    }
+    } // end of createEscrow
+
+    // Helper function to convert an address array to a payable address array
+    function _toPayableArray(address[] memory _addresses) private pure returns (address payable[] memory) {
+        address payable[] memory payableArray = new address payable[](_addresses.length);
+        for (uint256 i = 0; i < _addresses.length; i++) {
+            payableArray[i] = payable(_addresses[i]);
+        }
+        return payableArray;
+    } // end of _toPayableArray
 
     // Allows the sender or recipient to negotiate the amount to be released from the escrow.
     function negotiateEscrow(uint256 _escrowId, uint256 _negotiatedAmount) public {
@@ -47,18 +61,39 @@ contract Escrow is IEscrow {
         EscrowInfo storage escrow = escrows[_escrowId];
 
         require(!escrow.isFulfilled, "Escrow is already fulfilled.");
-        require(msg.sender == escrow.sender || msg.sender == escrow.recipient, "Only the sender or recipient can negotiate the escrow.");
+        require(msg.sender == escrow.sender || isRecipient(msg.sender, escrow.recipients), "Only the sender or recipient can negotiate the escrow.");
         require(_negotiatedAmount <= escrow.amount, "Negotiated amount must be less than or equal to the escrow amount.");
 
+        // Update the negotiated amount and distribute the funds accordingly
         escrow.negotiated_amount = _negotiatedAmount;
 
         uint256 remaining_amount = escrow.amount - escrow.negotiated_amount;
-        escrow.recipient.transfer(escrow.negotiated_amount);
+        distributeAmount(escrow.recipients, escrow.negotiated_amount);
         escrow.sender.transfer(remaining_amount);
 
         escrow.isFulfilled = true;
         emit EscrowFulfilled(_escrowId);
-    }
+    } // end of negotiateEscrow
+
+    // Distributes the specified amount equally among the recipients
+    function distributeAmount(address payable[] storage _recipients, uint256 _totalAmount) private {
+        uint256 numRecipients = _recipients.length;
+        uint256 amountPerRecipient = _totalAmount / numRecipients;
+
+        for (uint256 i = 0; i < numRecipients; i++) {
+            _recipients[i].transfer(amountPerRecipient);
+        }
+    } // end of distributeAmount
+
+    // Checks if the provided address is one of the recipients in the recipients array
+    function isRecipient(address _address, address payable[] storage _recipients) private view returns (bool) {
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            if (_address == _recipients[i]) {
+                return true;
+            }
+        }
+        return false;
+    } // end of isRecipient
 
     // Allows the sender to fulfill the escrow, releasing the negotiated or full amount to the recipient.
     function fulfillEscrow(uint256 _escrowId) public {
@@ -69,21 +104,27 @@ contract Escrow is IEscrow {
         require(msg.sender == escrow.sender, "Only the sender can fulfill the escrow.");
         
         uint256 finalAmount = escrow.negotiated_amount > 0 ? escrow.negotiated_amount : escrow.amount;
-        escrow.recipient.transfer(finalAmount);
+        distributeAmount(escrow.recipients, finalAmount);
 
         escrow.isFulfilled = true;
         emit EscrowFulfilled(_escrowId);
-    }
+    } // end of fulfillEscrow
 
     // Returns the current balance of the escrow contract.
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
-    }
+    } // end of getContractBalance
 
-    // Event triggered when an escrow is created.
-    event EscrowCreated(uint256 indexed escrowId, address indexed sender, address indexed recipient, uint256 amount);
+    // Returns the recipients of the specified escrow
+    function getEscrowRecipients(uint256 _escrowId) public view returns (address[] memory) {
+        require(_escrowId < nextEscrowId, "Invalid escrow ID.");
+        EscrowInfo storage escrow = escrows[_escrowId];
+        address[] memory recipients = new address[](escrow.recipients.length);
 
-    // Event triggered when an escrow is fulfilled.
-    event EscrowFulfilled(uint256 indexed escrowId);
-}
+        for (uint256 i = 0; i < escrow.recipients.length; i++) {
+            recipients[i] = escrow.recipients[i];
+        }
 
+        return recipients;
+    } // end of getEscrowRecipients
+} // end of Escrow
