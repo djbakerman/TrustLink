1eb77c48556d6125a3f9ac40e220f1ef1ad021b9
Daniel Molloy <dmolloy@myonlysolution.com>
2023-05-08 07:48:23 -0400

    improved KPI contract interaction

--- Escrow.sol
+++ Escrow.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: LicenseRef-Proprietary
 // see LICENSE in the source repository
-// working version
 
 pragma solidity ^0.8.0;
 
@@ -22,7 +21,11 @@ contract Escrow is IEscrow {
     mapping(uint256 => EscrowInfo) public escrows;
     uint256 public nextEscrowId;
 
+    // A mapping of escrowId to KPI Contracs
+    mapping(uint256 => address) public kpiContractAddresses;
+
     // Creates a new escrow with the provided recipients, amount, and KPIs, and stores it in the mapping.
+    // Returns the escrow ID.
     function createEscrow(address[] memory _recipients, uint256 _amount) public payable returns (uint256) {
         require(msg.value == _amount, "Amount sent does not match the specified amount.");
         require(_recipients.length > 0, "At least one recipient is required.");
@@ -56,6 +59,7 @@ contract Escrow is IEscrow {
     } // end of _toPayableArray
 
     // Allows the sender or recipient to negotiate the amount to be released from the escrow.
+    // Updates the negotiated amount and distributes the funds accordingly.
     function negotiateEscrow(uint256 _escrowId, uint256 _negotiatedAmount) public {
         require(_escrowId < nextEscrowId, "Invalid escrow ID.");
         EscrowInfo storage escrow = escrows[_escrowId];
@@ -64,7 +68,7 @@ contract Escrow is IEscrow {
         require(msg.sender == escrow.sender || isRecipient(msg.sender, escrow.recipients), "Only the sender or recipient can negotiate the escrow.");
         require(_negotiatedAmount <= escrow.amount, "Negotiated amount must be less than or equal to the escrow amount.");
 
-        // Update the negotiated amount and distribute the funds accordingly
+                // Update the negotiated amount and distribute the funds accordingly
         escrow.negotiated_amount = _negotiatedAmount;
 
         uint256 remaining_amount = escrow.amount - escrow.negotiated_amount;
@@ -96,24 +100,46 @@ contract Escrow is IEscrow {
     } // end of isRecipient
 
     // Allows the sender to fulfill the escrow, releasing the negotiated or full amount to the recipient.
-    function fulfillEscrow(uint256 _escrowId) public {
+    // Returns true if the escrow is successfully fulfilled.
+    function fulfillEscrow(uint256 _escrowId) public returns (bool) {
         require(_escrowId < nextEscrowId, "Invalid escrow ID.");
         EscrowInfo storage escrow = escrows[_escrowId];
 
         require(!escrow.isFulfilled, "Escrow is already fulfilled.");
-        require(msg.sender == escrow.sender, "Only the sender can fulfill the escrow.");
-        
+        require(msg.sender == escrow.sender || msg.sender == kpiContractAddresses[_escrowId], "Only the sender or the associated KPI contract can fulfill the escrow.");
+    
         uint256 finalAmount = escrow.negotiated_amount > 0 ? escrow.negotiated_amount : escrow.amount;
         distributeAmount(escrow.recipients, finalAmount);
 
         escrow.isFulfilled = true;
         emit EscrowFulfilled(_escrowId);
+
+        return true;
     } // end of fulfillEscrow
 
     // Returns the current balance of the escrow contract.
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     } // end of getContractBalance
+
+    // Check if the escrow is fulfilled
+    function isFulfilled(uint256 _escrowId) external view override returns (bool) {
+        require(_escrowId < nextEscrowId, "Escrow does not exist.");
+        return escrows[_escrowId].isFulfilled;
+    }
+
+    function getNextEscrowId() public view override returns (uint256) {
+        return nextEscrowId;
+    }   // end of getNextEscrowId
+
+    // Sets the KPI contract address associated with the specified escrow ID
+    function setKPIContractAddress(uint256 _escrowId, address _kpiContractAddress) public {
+        require(kpiContractAddresses[_escrowId] == address(0), "KPI contract address is already set for this escrow.");
+        kpiContractAddresses[_escrowId] = _kpiContractAddress;
+
+                // Emit the KPIContractAddressSet event
+        emit KPIContractAddressSet(_escrowId, _kpiContractAddress);
+    }
 
     // Returns the recipients of the specified escrow
     function getEscrowRecipients(uint256 _escrowId) public view returns (address[] memory) {
