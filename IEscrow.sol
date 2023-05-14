e7b2e986580915e383aaf22789da7ee1b65b70dd
Daniel Molloy <dmolloy@myonlysolution.com>
2023-05-14 09:12:37 -0400

    Update IEscrow.sol

--- IEscrow.sol
+++ IEscrow.sol
@@ -14,5 +14,6 @@ interface IEscrow {
     function fulfillEscrow(uint256 _escrowId) external returns (bool);
     function setRecipientAgrees(uint256 _escrowId, bool _agrees) external;
     function getRecipientAgrees(uint256 _escrowId, address _recipient) external view returns (bool);
+    function getNextEscrowId() external view returns (uint256);
     function areAllRecipientsAgreed(uint256 _escrowId) external view returns (bool);
 }
