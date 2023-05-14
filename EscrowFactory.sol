268a06c4905e1f274d37bb49ace36e27cd181cfa
Daniel Molloy <dmolloy@myonlysolution.com>
2023-05-13 20:00:06 -0400

    Update EscrowFactory.sol

--- EscrowFactory.sol
+++ EscrowFactory.sol
@@ -3,35 +3,26 @@
 
 pragma solidity ^0.8.0;
 
-import "./IEscrow.sol";
 import "./Escrow.sol";
 
-// The EscrowFactory contract is a factory contract that deploys instances of the Escrow contract.
 contract EscrowFactory {
-    // A mapping to store all deployed Escrow contracts with their respective sender addresses.
-    mapping(address => address[]) public escrowContracts;
+    mapping(address => Escrow) public userEscrows;
+    uint256 public nextUserId;
 
-    // Event emitted when a new Escrow contract is created
-    event EscrowContractCreated(address indexed sender, address escrowAddress);
+    event UserEscrowCreated(address indexed user, address indexed escrow);
 
-    // Creates a new Escrow contract with the provided recipients and amount and stores its address in the mapping.
-    function createEscrowContract(address[] memory _recipients, uint256 _amount) public payable returns (address) {
-        require(msg.value == _amount, "Amount sent does not match the specified amount.");
-        require(_recipients.length > 0, "At least one recipient is required.");
-
-        // Deploy a new Escrow contract
-        Escrow newEscrow = new Escrow{value: msg.value}(_recipients, _amount, msg.sender);
-
-        // Store the deployed Escrow contract address in the mapping
-        escrowContracts[msg.sender].push(address(newEscrow));
-
-        emit EscrowContractCreated(msg.sender, address(newEscrow));
-
-        return address(newEscrow);
+    function getOrCreateUserEscrow() public returns (Escrow) {
+        Escrow userEscrow = userEscrows[msg.sender];
+        if (address(userEscrow) == address(0)) {
+            userEscrow = new Escrow();
+            userEscrows[msg.sender] = userEscrow;
+            emit UserEscrowCreated(msg.sender, address(userEscrow));
+            nextUserId++;
+        }
+        return userEscrow;
     }
 
-    // Returns the list of deployed Escrow contracts for a specific sender address
-    function getDeployedEscrows(address _sender) public view returns (address[] memory) {
-        return escrowContracts[_sender];
+    function getNumberEscrowUsers() public view returns (uint256) {
+        return (nextUserId);
     }
 }
