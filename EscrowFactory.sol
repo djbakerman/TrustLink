f3c6d5ed04a9bd87601b43975af219b5bd99c2a3
Daniel Molloy <dmolloy@myonlysolution.com>
2023-05-18 07:21:46 -0400

    Update EscrowFactory.sol

--- EscrowFactory.sol
+++ EscrowFactory.sol
@@ -8,13 +8,18 @@ import "./Escrow.sol";
 contract EscrowFactory {
     mapping(address => Escrow) public escrowAccounts;
     uint256 public nextAccountId;
+    address public kpiFactoryAddress; // The deployed address of the KPIFactory contract
+
+    constructor(address _kpiFactoryAddress) {
+        kpiFactoryAddress = _kpiFactoryAddress;
+    }
 
     event UserEscrowCreated(address indexed user, address indexed escrow);
 
     function getOrCreateEscrowAccount() public returns (Escrow) {
         Escrow userEscrow = escrowAccounts[msg.sender];
         if (address(userEscrow) == address(0)) {
-            userEscrow = new Escrow();
+            userEscrow = new Escrow(kpiFactoryAddress); // Pass the KPIFactory address to the Escrow constructor
             escrowAccounts[msg.sender] = userEscrow;
             emit UserEscrowCreated(msg.sender, address(userEscrow));
             nextAccountId++;
