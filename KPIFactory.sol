1cc856af44b6ba28685a342b2e44d77b73901b18
Daniel Molloy <dmolloy@myonlysolution.com>
2023-06-02 08:27:16 -0400

    Update KPIFactory.sol

--- KPIFactory.sol
+++ KPIFactory.sol
@@ -4,15 +4,25 @@
 pragma solidity ^0.8.0;
 
 import "./KPI.sol";
+import "./KPIProxy.sol";
 
 contract KPIFactory {
     mapping(address => mapping(uint256 => address)) public kpiContracts;
+    address public kpiProxyAddress;
+
     event KPIContractCreated(uint256 indexed escrowId, address indexed escrowAddress, address kpiContract);
 
-    function getOrCreateKPIForEscrow(uint256 _escrowId, address _escrowAddress) public returns (address) {
+    constructor(address _kpiProxyAddress) {
+        kpiProxyAddress = _kpiProxyAddress;
+    }
+
+    function getOrCreateKPIForEscrow(uint256 _escrowId, address _escrowAddress, address _sender) public returns (address) {
         if (kpiContracts[_escrowAddress][_escrowId] == address(0)) {
-            KPI newKPI = new KPI(_escrowAddress);
+            KPI newKPI = new KPI(_escrowAddress, _sender);
             kpiContracts[_escrowAddress][_escrowId] = address(newKPI);
+
+            // Register the new KPI contract with the KPIProxy contract
+            KPIProxy(kpiProxyAddress).registerKPI(newKPI);
 
             // Emit the event when a new KPI contract is created
             emit KPIContractCreated(_escrowId, _escrowAddress, address(newKPI));
