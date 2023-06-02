b2ddd7827c2bc651a2776c53ff8c86cf5096ad89
Daniel Molloy <dmolloy@myonlysolution.com>
2023-06-02 08:29:19 -0400

    updated with comments

--- IKPIFactory.sol
+++ IKPIFactory.sol
@@ -3,11 +3,33 @@
 
 pragma solidity ^0.8.0;
 
-// IKPIFactory is an interface to define the required functions for the KPIFactory contract.
-interface IKPIFactory {
-    // Creates a new KPI contract instance for the sender.
-    function createKPIContract() external returns (address);
+// This is the interface for the Escrow contract
+interface IEscrow {
+    // Event emitted when a new escrow is created
+    event EscrowCreated(uint256 indexed escrowId, address indexed sender, uint256 amount);
+    // Event emitted when an escrow is fulfilled
+    event EscrowFulfilled(uint256 indexed escrowId);
+    // Event emitted when a new recipient is added to an escrow
+    event RecipientAdded(uint256 indexed escrowId, address indexed recipient);
+    // Event emitted when a recipient changes their agreement status
+    event RecipientAgreementChanged(uint256 indexed escrowId, address indexed recipient, bool agrees);
 
-    // Event emitted when a new KPI contract is created.
-    event KPICreated(address indexed kpiAddress, address indexed creator);
-}
+    // Creates a new escrow with the given recipients and amount
+    function createEscrow(address[] memory _recipients, uint256 _amount) external payable returns (uint256);
+    // Negotiates the amount of an existing escrow
+    function negotiateEscrow(uint256 _escrowId, uint256 _negotiatedAmount) external;
+    // Fulfills an existing escrow
+    function fulfillEscrow(uint256 _escrowId) external returns (bool);
+    // Checks if an escrow is fulfilled
+    function isEscrowFulfilled(uint256 _escrowId) external view returns (bool);
+    // Sets the agreement status of a recipient for a given escrow
+    function setRecipientAgrees(uint256 _escrowId, bool _agrees) external;
+    // Gets the agreement status of a recipient for a given escrow
+    function getRecipientAgrees(uint256 _escrowId, address _recipient) external view returns (bool);
+    // Gets the next escrow ID
+    function getNextEscrowId() external view returns (uint256);
+    // Checks if all recipients of an escrow have agreed
+    function areAllRecipientsAgreed(uint256 _escrowId) external view returns (bool);
+    // Gets or creates a KPI for a given escrow
+    function getOrCreateKPIForEscrow(uint256 _escrowId) external returns (address);
+} IEscrow
