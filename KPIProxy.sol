33feef8c06d65c80c397425882390871a866ebf8
Daniel Molloy <dmolloy@myonlysolution.com>
2023-06-02 21:31:03 -0400

    updated with comments

--- KPIProxy.sol
+++ KPIProxy.sol
@@ -8,31 +8,41 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 import "./KPI.sol";
 
+// This contract is a proxy for managing KPI contracts and their upkeep
 contract KPIProxy is KeeperCompatibleInterface,ChainlinkClient, ConfirmedOwner {
+    // Array to store all registered KPI contracts
     KPI[] public kpis;
+    // The last time the KPIs were fetched
     uint256 public lastFetchTime;
+    // The minimum time between fetches
     uint256 public minTimeBetweenFetches = 15 minutes;
+    // Boolean to pause or unpause the contract
     bool public paused = false;
 
     using Chainlink for Chainlink.Request;
 
+    // Constructor sets the owner of the contract
     constructor() ConfirmedOwner(msg.sender) {
     }
 
+    // Function to register a new KPI contract
     function registerKPI(KPI kpi) external {
         kpis.push(kpi);
     }
 
+    // Function to check if upkeep is needed
     function checkUpkeep(bytes calldata checkData) external view override returns (bool upkeepNeeded, bytes memory performData) {
-        //pause the function if we don't want it running anymore
+        // Pause the function if we don't want it running anymore
         if (paused) {
             return (false, "");
         }
 
+        // Upkeep is needed if the time since the last fetch is greater than the minimum time between fetches
         upkeepNeeded = (block.timestamp - lastFetchTime) > minTimeBetweenFetches;
         performData = checkData; // Pass the checkData to performUpkeep
     }
 
+    // Function to withdraw LINK tokens
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(
@@ -41,24 +51,28 @@ contract KPIProxy is KeeperCompatibleInterface,ChainlinkClient, ConfirmedOwner {
         );
     }
 
+    // Function to pause the contract
     function pause() public onlyOwner {
         paused = true;
     }
 
+    // Function to unpause the contract
     function unpause() public onlyOwner {
         paused = false;
     }
 
+    // Function to perform upkeep
     function performUpkeep(bytes calldata /*performData*/) external override {
-    // Loop over all registered KPI contracts and fetch each kpi values therein
-    for (uint i = 0; i < kpis.length; i++) {
-        KPI kpi = kpis[i];
-        for (uint256 j = 0; j < kpi.getEscrowKPIs(i).length; j++) {
-            bytes32 kpiId = kpi.getEscrowKPIs(i)[j];
-            kpi.callFetchKPIPointValue(kpiId);
+        // Loop over all registered KPI contracts and fetch each KPI value therein
+        for (uint i = 0; i < kpis.length; i++) {
+            KPI kpi = kpis[i];
+            for (uint256 j = 0; j < kpi.getEscrowKPIs(i).length; j++) {
+                bytes32 kpiId = kpi.getEscrowKPIs(i)[j];
+                kpi.callFetchKPIPointValue(kpiId);
+            }
         }
-    }
 
-    lastFetchTime = block.timestamp; // Update the lastFetchTime to the current timestamp
-}
-}
+        // Update the lastFetchTime to the current timestamp
+        lastFetchTime = block.timestamp;
+    }
+} // KPIProxy
