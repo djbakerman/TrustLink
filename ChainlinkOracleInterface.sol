e21c14603d025dd5a2bac7e197cf41eab847f1d6
Daniel Molloy <dmolloy@myonlysolution.com>
2023-06-04 15:53:49 -0400

    Delete ChainlinkOracleInterface.sol

--- ChainlinkOracleInterface.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: LicenseRef-Proprietary
-// see LICENSE in the source repository
-
-pragma solidity ^0.8.0;
-
-interface ChainlinkOracleInterface {
-    function requestData(string calldata _url) external returns (uint256);
-}
