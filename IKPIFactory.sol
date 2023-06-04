e18ddbbd5c9170a3385d5fff9a17c29c167ff91c
Daniel Molloy <dmolloy@myonlysolution.com>
2023-06-04 15:56:17 -0400

    final hackathon version

--- IKPIFactory.sol
+++ IKPIFactory.sol
@@ -3,30 +3,11 @@
 
 pragma solidity ^0.8.0;
 
-import "./KPI.sol";
+// IKPIFactory is an interface to define the required functions for the KPIFactory contract.
+interface IKPIFactory {
+    // Creates a new KPI contract instance for the sender.
+    function createKPIContract() external returns (address);
 
-// This contract is a factory for creating and managing KPI contracts
-contract KPIFactory {
-    // Mapping to store KPI contracts for each escrow
-    mapping(address => mapping(uint256 => address)) public kpiContracts;
-
-    // Event emitted when a new KPI contract is created
-    event KPIContractCreated(uint256 indexed escrowId, address indexed escrowAddress, address kpiContract);
-
-    // Function to get or create a KPI contract for a given escrow
-    function getOrCreateKPIForEscrow(uint256 _escrowId, address _escrowAddress, address _sender) public returns (address) {
-        // If there is no KPI contract for the given escrow, create a new one
-        if (kpiContracts[_escrowAddress][_escrowId] == address(0)) {
-            KPI newKPI = new KPI(_escrowAddress, _sender);
-            kpiContracts[_escrowAddress][_escrowId] = address(newKPI);
-
-            // Emit the event when a new KPI contract is created
-            emit KPIContractCreated(_escrowId, _escrowAddress, address(newKPI));
-
-            return address(newKPI);
-        } else {
-            // If a KPI contract for the given escrow already exists, return its address
-            return kpiContracts[_escrowAddress][_escrowId];
-        }
-    }
-} // KPIFactory
+    // Event emitted when a new KPI contract is created.
+    event KPICreated(address indexed kpiAddress, address indexed creator);
+}
